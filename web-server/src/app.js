471e2d3518b24dabc7b571b542548efaf6dc13e1
md syedul arif <raiak_77@yahoo.com>
2021-05-22 13:30:01 +0300

    adding geocode and forecast function to weather app and implementedd query parameter for taking addresss from url

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,38 @@
 const path = require('path');
-
+const hbs = require('hbs');
 const express = require('express');
 
-console.log(__dirname);
-console.log(path.join(__dirname, '../public'));
+
+const geocode = require('./utils/geoCode');
+const forecast = require('./utils/foreCast');
+
+
+// console.log(__dirname);
+//  console.log(path.join(__dirname, '../public'));
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public');
 
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../template/views');
+const partialsPath = path.join(__dirname, '../template/partials');
+
+
+//setup handlebar and views location
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+// app.set('views', path.join(__dirname, '../views'));
 
-app.use(express.static(path.join(publicDirPath)));
+//Setup static directory
+app.use(express.static(path.join(publicDirectoryPath)));
 
 
 app.get('', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Weather ', name: 'Arif' });
+});
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About page ', name: 'Arif' });
 });
 
 /**When someone tries to get something at a specific route, we set that up 
@@ -27,12 +44,9 @@ app.get('', (req, res) => {
 
 /* And it also takes in a function.Now the function is where we describe 
 what we want to do when someone visits this particular route. */
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'arif',
-//         roll: '119'
-//     });
-// });
+app.get('/help', (req, res) => {
+    res.render('help', { title: 'Help ', name: 'Arif' });
+});
 
 /*Now, this function gets called with two very important arguments.
 The first is an object containing information about the incoming 
@@ -45,10 +59,56 @@ to customize what we're going to send back to the requester. */
 //     res.send('<h1>About page node js</h1>');
 // });
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'it is raining',
-        location: 'helsinki'
+    const address = req.query.address;
+    if (!address) {
+        return res.send({ error: 'must provide --address' });
+    }
+    geocode(address, (error, { lat, lng, loc } = {}) => {
+        if (error) {
+            return res.send({ error: 'Address not found' });
+        }
+        forecast(lng, lat, (error, foreCastdata) => {
+            if (error) {
+                return res.send({ error: error });
+            }
+            res.send({
+                forecast: foreCastdata,
+                location: loc,
+                address: req.query.address
+            });
+        });
     });
+
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({ error: 'must provide a serch term' });
+    }
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMessage: ' help page not found',
+        title: '404 ',
+        name: 'Arif'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'page not found',
+        title: '404 ',
+        name: 'Arif'
+    });
+});
+
+
+app.get('', () => {
+
 });
 
 /*Now the other optional argument we can pass to the listen method is a 
